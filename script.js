418209bc01bfb4a04f90f3341ea10366f0c79653
1nfer <cristhian30050@gmail.com>
2025-02-21 14:53:18 -0500

    contenido

---
+++ script.js
@@ -0,0 +1,21 @@
+// Sidebar
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSidebar() {
+    sidebar.classList.toggle('close')
+
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    })
+}
+
+// Mostrar/Ocultar sub-menus
+function toggleSubMenu(button) {
+    button.nextElementSibling.classList.toggle('show');
+
+    if(sidebar.classList.contains('close')) {
+        sidebar.classList.toggle('close')
+    }
+}
